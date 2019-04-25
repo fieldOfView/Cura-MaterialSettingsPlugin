e0ab34a9a9450d28622f2601b3b2c8647e377ee9
fieldOfView <aldo@fieldofview.com>
2019-04-25 13:46:28 +0200

    Use preference to store visible material settings

--- MaterialSettingsPlugin.py
+++ MaterialSettingsPlugin.py
@@ -10,6 +10,22 @@ class MaterialSettingsPlugin(Extension):
     def __init__(self):
         super().__init__()
 
+        default_material_settings = {
+            "default_material_print_temperature",
+            "default_material_bed_temperature",
+            "material_standby_temperature",
+            #"material_flow_temp_graph",
+            "cool_fan_speed",
+            "retraction_amount",
+            "retraction_speed",
+            "material_flow",
+        }
+
+        Application.getInstance().getPreferences().addPreference(
+            "material_settings/visible_settings",
+            ";".join(default_material_settings)
+        )
+
         Application.getInstance().engineCreatedSignal.connect(self._onEngineCreated)
 
     def _onEngineCreated(self):
--- MaterialSettingsPluginVisibilityHandler.py
+++ MaterialSettingsPluginVisibilityHandler.py
@@ -1,22 +1,28 @@
 # Copyright (c) 2019 fieldOfView
 # The MaterialSettingsPlugin is released under the terms of the AGPLv3 or higher.
 
-import UM.Settings.Models.SettingVisibilityHandler
+from UM.Settings.Models.SettingVisibilityHandler import SettingVisibilityHandler
+from UM.Application import Application
 
-
-class MaterialSettingsPluginVisibilityHandler(UM.Settings.Models.SettingVisibilityHandler.SettingVisibilityHandler):
+class MaterialSettingsPluginVisibilityHandler(SettingVisibilityHandler):
     def __init__(self, parent = None, *args, **kwargs):
         super().__init__(parent = parent, *args, **kwargs)
 
-        material_settings = {
-            "default_material_print_temperature",
-            "default_material_bed_temperature",
-            "material_standby_temperature",
-            #"material_flow_temp_graph",
-            "cool_fan_speed",
-            "retraction_amount",
-            "retraction_speed",
-            "material_flow",
-        }
+        self._preferences = Application.getInstance().getPreferences()
+        self._preferences.preferenceChanged.connect(self._onPreferencesChanged)
+
+        self._onPreferencesChanged("material_settings/visible_settings")
+
+
+    def _onPreferencesChanged(self, name: str) -> None:
+        if name != "material_settings/visible_settings":
+            return
+
+        visibility_string = self._preferences.getValue("material_settings/visible_settings")
+        if not visibility_string:
+            self._preferences.resetPreference("material_settings/visible_settings")
+            return
+
+        material_settings = set(visibility_string.split(";"))
 
         self.setVisible(material_settings)
